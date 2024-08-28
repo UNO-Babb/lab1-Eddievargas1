33e41ea920b93636aacdad0afae165aeef9bd513
Eddievargas1 <eddievargas@unomaha.edu>
2024-08-28 16:40:37 +0000

    first udate

--- FirstProgram.py
+++ FirstProgram.py
@@ -6,16 +6,18 @@
 def main():
   print("First Program")
   #Say hello
-  
+  print("Hello") 
   #Ask for the user's name
-
+  name = input("What is your name? ")
   #Use the user's name in the program.
-
+  print("Hi", name)
   #Ask the user for their age.
-
+  age = input("How old are you? ")
   #Tell the user what year they were born in.
+  age = int(age)
+  birthYear = 2024 - age 
   #Assume that they have not had their birthday yet this year.
-
+  print(birthYear)
 
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
